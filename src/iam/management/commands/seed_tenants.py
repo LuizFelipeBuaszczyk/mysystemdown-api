7646787f63df15bbd81cece333f55d54a259a1b3
Luiz Felipe Buaszczyk <luizfelipebuaszczyk@gmail.com>
2026-02-13 13:43:57 -0300

    chore: Validação ao inserir a seed_tenant inicial, garantindo que caso a aplicação suba e já exista a tenant não tentará recriar

--- src/iam/management/commands/seed_tenants.py
+++ src/iam/management/commands/seed_tenants.py
@@ -3,6 +3,10 @@ from tenants.models import Domain, Client
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        if Client.objects.filter(schema_name="public").exists():
+            self.stdout.write(self.style.SUCCESS("Tenant already exists"))
+            return
+        
         tenant = Client.objects.create(schema_name="public", name="Public")
         domain = Domain.objects.create(domain="localhost", tenant=tenant, is_primary=True)
 
